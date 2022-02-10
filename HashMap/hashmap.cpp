d1bc72e624290be471f3ade717d1d338eb779702
John <thatbotjohn@gmail.com>
2022-02-10 19:35:25 +0000

    Initial Commit

---
+++ HashMap/hashmap.cpp
@@ -0,0 +1,6 @@
+//
+// Created by john on 2/10/22.
+//
+
+#include "hashmap.h"
+
---
+++ HashMap/hashmap.h
@@ -0,0 +1,8 @@
+//
+// Created by john on 2/10/22.
+//
+
+#ifndef STORELIB_HASHMAP_H
+#define STORELIB_HASHMAP_H
+
+#endif //STORELIB_HASHMAP_H
