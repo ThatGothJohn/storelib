eb4beb9fe78bdbfdff93107962e2dbb50093c0a9
John <thatbotjohn@gmail.com>
2022-02-13 03:05:17 +0000

    Implemented a simple hashing algorithm Also started to implement the hashmap functionality

--- HashMap/hashmap.h
+++ HashMap/hashmap.h
@@ -8,6 +8,21 @@
 #include <cstdlib>
 #include <cstring>
 #include <cassert>
+#include <bit>
+
+//https://lemire.me/blog/2018/08/15/fast-strongly-universal-64-bit-hashing-everywhere/
+template<typename T>
+u_int64_t simple_hash(T data){
+    u_int32_t a = 0xFF5242A3,b=0x123BF069,c=0xDEADBEEF; //magic constants
+
+    auto sized_data = (u_int64_t) data;
+//    int low = (int)x;
+    auto low = (u_int32_t) sized_data;
+//    int high = (int)(x >>> 32);
+    auto high = (u_int32_t) (sized_data >> 32);
+//    return (int)((a * low + b * high + c) >>> 32);
+    return std::rotr((a * low + b * high + c), 32);
+}
 
 template<typename T,typename Y>
 struct hashmap {
@@ -16,9 +31,13 @@ public:
     explicit hashmap(size_t);
     Y& operator[](size_t);
     ~hashmap() = default;
+    void put(T, Y);
+    void remove(T);
 private:
     Y* mem;
     size_t length;
+
+    size_t get_index_for(T);
 };
 
 template<typename T, typename Y>
@@ -26,7 +45,7 @@ hashmap<T, Y>::hashmap(size_t len) {
     this->mem = (Y*)malloc(len * sizeof(Y));
     if (this->mem != nullptr) {
         this->length = len;
-        memset(this->mem, 42, len);
+        memset(this->mem, 0, len);
     }
     else
         this->length = 0;
@@ -39,6 +58,25 @@ template<typename T, typename Y>
 Y& hashmap<T,Y>::operator[](size_t index){
     assert(index<this->length); //-Werror=array-bounds will catch this, but best to stay safe
     return *(this->mem + index);
+}
+
+template<typename T, typename Y>
+size_t hashmap<T,Y>::get_index_for(T key){
+    auto hash = simple_hash(key);
+    return hash%this->length;
+}
+
+template<typename T, typename Y>
+void hashmap<T, Y>::put(T key, Y data) {
+    //fixme: implement resizing
+    auto index = this->get_index_for(key);
+    this->mem[index] = data;
+}
+
+template<typename T, typename Y>
+void hashmap<T, Y>::remove(T key) {
+    auto index = this->get_index_for(key);
+    this->mem[index] = (Y)NULL;
 }
 
 #endif //STORELIB_HASHMAP_H
--- test.cpp
+++ test.cpp
@@ -6,13 +6,24 @@
 
 int main(int argc, char** argv){
     (void)argc, (void)argv;
-    size_t len = 10;
-    hashmap<int,size_t> b(len);
-    for (size_t x =0;x<len;x++)
-        b[x] = x;
+    size_t len = 30;
+    hashmap<char,size_t> b(len);
+
+    const char wa[] = "1234uboc:{:@];[;q9uopwQWE";
+    for (char ch : wa) {
+        b.put(ch, 42);
+    }
 
     for (size_t x =0;x<len;x++)
         printf("%lu\n", b[x]);
+
+    auto w = simple_hash("oh no!");
+    auto x= simple_hash(52);
+    auto y = simple_hash("oh no!");
+    auto z = simple_hash(0x1234FF5432198765);
+
+    printf("\nSimple hash test: \n\"oh no!\" : %#lX \n52 : %#lX \n\"oh no!\" : %#lX \n0x1234FF5432198765 : %#lX\n%lu\n",w, x,y,z, sizeof (z));
+
 
     return 0;
 }
